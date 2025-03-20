1cdf7e44c3b4d3d0748cf67ea48b9385e9ba25e8
ReiKoka <reikoka1@outlook.com>
2025-03-20 23:34:02 +0100

    Added Services

--- src/components/ui/SinglePhoto.tsx
+++ src/components/ui/SinglePhoto.tsx
@@ -10,9 +10,9 @@ function SinglePhoto({ flex, text, imageUrl }: SinglePhotoProps) {
       className={`animate-fade-up animate-once animate-duration-1000 animation-delay-500 ${flex} animate-ease-out relative w-full flex-1 rounded-4xl`}
       style={{ backgroundImage: `url(${imageUrl})`, backgroundSize: "cover" }}
     >
-      <div className="absolute top-0 left-0 h-full w-full rounded-4xl bg-black opacity-25"></div>
+      <div className="absolute top-0 left-0 h-full w-full rounded-4xl bg-black opacity-35"></div>
       <div className="absolute top-0 left-0 flex h-full w-full items-center justify-center">
-        <h1 className="font-primary text-background dark:text-foreground text-4xl font-bold capitalize">
+        <h1 className="font-primary text-background dark:text-foreground text-4xl leading-0 font-bold capitalize">
           {text}
         </h1>
       </div>
