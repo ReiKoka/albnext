25f0c25991b2caa362141949e10bb8ef31f84b1e
ReiKoka <reikoka1@outlook.com>
2025-03-22 20:43:46 +0100

    style-fix-mobile

--- src/components/ui/mobile/SingleSlideMobile.tsx
+++ src/components/ui/mobile/SingleSlideMobile.tsx
@@ -15,7 +15,7 @@ function SingleSlideMobile({
 }: SingleSlideMobileProps) {
   return (
     <div className="h-full focus-within:outline-0">
-      <div className="shadow-slide-2 dark:shadow-toast flex h-full flex-col justify-between gap-4 rounded-xl p-4 ss:p-5 focus-visible:outline-0">
+      <div className="shadow-slide-2 dark:shadow-toast flex h-full flex-col justify-between rounded-xl p-4 ss:p-5 focus-visible:outline-0">
         <div className="mx-auto flex aspect-square h-24 w-24 items-center justify-center rounded-xl bg-[url('/gradient-background.png')] bg-cover bg-center ss:h-28 ss:w-28">
           <Lottie animationData={icon} loop={true} />
         </div>
