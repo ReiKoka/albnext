33f114bf618ce7d69317b8527f0dcdb84f127947
ReiKoka <reikoka1@outlook.com>
2025-03-22 23:47:33 +0100

    style-fix-web

--- src/components/main/Home/WhyAlbaniaSection.tsx
+++ src/components/main/Home/WhyAlbaniaSection.tsx
@@ -10,7 +10,7 @@ function WhyAlbaniaSection() {
   const { navHeight } = useNav();
   const matches = useMediaQuery("(min-width: 768px)");
 
-  const WEB_OPTIONS: EmblaOptionsType = { loop: true };
+  const WEB_OPTIONS: EmblaOptionsType = { loop: true, align: "center" };
   const MOBILE_OPTIONS: EmblaOptionsType = { axis: "y", loop: true };
 
   const PLUGINS = [
@@ -27,12 +27,12 @@ function WhyAlbaniaSection() {
       style={{ height: `calc(100svh - ${navHeight}px)` }}
       className="text-foreground bg-muted flex flex-col items-center overflow-hidden p-4 lg:py-6"
     >
-      <div className="flex h-full flex-col gap-6 md:gap-2 w-full">
+      <div className="flex h-full w-full flex-col gap-6 md:gap-2">
         <h1 className="font-primary text-center text-3xl font-light md:text-4xl lg:text-5xl">
           Why invest in Albania
         </h1>
-        <div className="flex h-full max-h-full overflow-hidden grow flex-col justify-between gap-6 xs:gap-12">
-          <div className="flex flex-col gap-2 xs:gap-8">
+        <div className="xs:gap-12 flex h-full max-h-full grow flex-col justify-between gap-6 overflow-hidden md:gap-0">
+          <div className="xs:gap-8 flex flex-col gap-2">
             <h3 className="font-primary text-md px-10 text-center font-normal text-balance lg:text-lg">
               Where{" "}
               <span className="font-secondary animate-thrice animate-duration-[1000ms] animate-delay-0 animate-ease-out text-primary inline-block animate-bounce">
@@ -48,7 +48,7 @@ function WhyAlbaniaSection() {
               climate for ventures of all sizes.
             </p>
           </div>
-          <div className="flex h-full max-h-full overflow-hidden grow flex-col">
+          <div className="flex h-full max-h-full grow flex-col overflow-hidden">
             {matches ? (
               <WhyAlbaniaWebSlider
                 slides={sliderData}
--- src/components/ui/web/SingleSlideWeb.tsx
+++ src/components/ui/web/SingleSlideWeb.tsx
@@ -8,14 +8,14 @@ type SingleSlideWebProps = {
 
 function SingleSlideWeb({ icon, title, text }: SingleSlideWebProps) {
   return (
-    <div className="h-fit focus-within:outline-0">
+    <div className="h-full focus-within:outline-0">
       <div className=" flex h-full justify-between gap-8 rounded-xl p-4 focus-visible:outline-0">
         <div className="my-auto flex aspect-square h-36 w-36 items-center justify-center rounded-xl bg-[url('/gradient-background.png')] bg-cover bg-center shadow-box">
           <Lottie animationData={icon} loop={true} />
         </div>
 
         <div className="flex flex-col my-auto gap-4">
-          <h2 className="text-foreground font-primary text-right text-lg leading-5 font-bold">
+          <h2 className="text-foreground font-primary text-right text-balance text-lg leading-5 font-bold">
             {title}
           </h2>
           <p className="font-primary text-right text-[10px] leading-3 font-light">{text}</p>
