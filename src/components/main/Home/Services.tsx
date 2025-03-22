4322d784712cdf17dc701ac1d18d8ebbf918e2e7
ReiKoka <reikoka1@outlook.com>
2025-03-22 01:25:36 +0100

    Added more media queries

--- src/components/main/Home/Services.tsx
+++ src/components/main/Home/Services.tsx
@@ -1,36 +1,43 @@
+import { useMediaQuery } from "usehooks-ts";
 import useNav from "../../../hooks/useNav";
 import SinglePhoto from "../../ui/SinglePhoto";
 
 function Services() {
   const { navHeight } = useNav();
+  const matches = useMediaQuery("(min-width: 768px");
 
   return (
     <section
       style={{ height: `calc(100dvh - ${navHeight}px)` }}
-      className="text-foreground bg-muted flex flex-col items-center gap-8 p-8 md:p-10 lg:p-12"
+      className="text-foreground bg-muted flex flex-col items-center gap-4 p-4 lg:py-8"
     >
       <div className="flex h-full w-full flex-col gap-8">
         <h1 className="font-primary text-center text-3xl font-light md:text-4xl lg:text-5xl">
           Our Services
         </h1>
-        <div className="flex w-full grow flex-col items-center justify-between gap-6 md:grid md:grid-cols-2 md:grid-rows-[10px_1fr_1fr_10px]">
+        <div className="flex w-full max-w-[70%] xl:max-w-[50%] mx-auto grow flex-col items-center justify-between gap-6 md:grid md:grid-cols-2 md:grid-rows-[10px_1fr_1fr_10px]">
           <SinglePhoto
             flex={2}
-            text="real estate"
-            imageUrl="/services-1.png"
+            title="real estate"
+            text="Tap into Albania’s booming property sector with a partner who knows the landscape from the inside out. Our premium, end-to-end service covers everything from scouting exclusive off-market opportunities to managing post-purchase logistics. By combining meticulous research with real-world expertise, we deliver property investments designed for lasting returns."
+            imageUrl={!matches ? "/services-1.png" : "/services-1-web.png"}
             divClassName="md:row-start-1 md:row-end-5 h-full"
+            h1ClassName="top-[51%] 2xl:top-[85%] 2xl:left-[70%]"
+            pClassName=""
           />
 
           <SinglePhoto
             flex={1}
-            text="marketing"
+            title="marketing"
+            text=""
             imageUrl="/services-2.jpeg"
             divClassName="md:h-full md:row-start-2"
           />
 
           <SinglePhoto
             flex={1}
-            text="Business Setup"
+            title="Business Setup"
+            text=""
             imageUrl="/services-3.jpeg"
             divClassName="md:h-full md:pb-8 md:row-start-3"
           />
--- src/components/ui/SinglePhoto.tsx
+++ src/components/ui/SinglePhoto.tsx
@@ -2,19 +2,27 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 type SinglePhotoProps = {
+  title: string;
   text: string;
   imageUrl: string;
   divClassName?: string;
+  h1ClassName?: string;
   pClassName?: string;
   flex: number;
 };
 
 //prettier-ignore
-function SinglePhoto({ text, imageUrl, divClassName, pClassName, flex}: SinglePhotoProps) {
-  const pBaseStyles =
-    "font-primary text-background dark:text-foreground text-4xl font-bold capitalize absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-full text-center";
-  const pStyles = twMerge(clsx(pBaseStyles, pClassName));
+function SinglePhoto({ title, text, imageUrl, divClassName, h1ClassName, pClassName, flex}: SinglePhotoProps) {
+  // Paragraph Styles
+  const pBaseStyles = "font-primary text-background dark:text-foreground text-[6px] font-light absolute hidden"
+  const pStyles = twMerge(clsx(pBaseStyles, pClassName))
 
+  // H1 Styles
+  const h1BaseStyles =
+    "font-primary text-background dark:text-foreground text-4xl font-bold capitalize absolute top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] w-full text-center";
+  const h1Styles = twMerge(clsx(h1BaseStyles, h1ClassName));
+
+  // Div Styles
   const divBaseStyles = "relative w-full rounded-4xl";
   const divStyles = twMerge(clsx(divBaseStyles, divClassName))
 
@@ -28,8 +36,8 @@ function SinglePhoto({ text, imageUrl, divClassName, pClassName, flex}: SinglePh
       }}
     >
       <div className="absolute top-0 left-0 h-full w-full rounded-4xl bg-black opacity-35"></div>
-
-      <h1 className={pStyles}>{text}</h1>
+      <p className={pStyles}>{text}</p>
+      <h1 className={h1Styles}>{title}</h1>
     </div>
   );
 }
