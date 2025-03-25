97c32dc04290260d59973857dceabd55806a7940
ReiKoka <reikoka1@outlook.com>
2025-03-25 01:35:14 +0100

    Fixed services

--- src/components/main/Home/Services.tsx
+++ src/components/main/Home/Services.tsx
@@ -9,7 +9,7 @@ function Services() {
   return (
     <section
       style={{ "--nav-height": `${navHeight}px` } as React.CSSProperties}
-      className="text-foreground bg-muted flex h-[calc(100svh_-_var(--nav-height))] flex-col items-center gap-4 p-4 md:py-6 lg:py-8 landscape:h-[calc(120svh_-_var(--nav-height))]"
+      className="text-foreground bg-muted flex h-[calc(100svh_-_var(--nav-height))] flex-col items-center gap-4 p-4 md:py-6 lg:py-8 landscape:h-[calc(120svh_-_var(--nav-height))] landscape:lg:h-[calc(100svh_-_var(--nav-height))]"
     >
       <div className="flex h-full w-full flex-col gap-4 overflow-hidden">
         <h1 className="font-primary text-center text-3xl font-light md:text-4xl lg:text-5xl">
@@ -21,7 +21,6 @@ function Services() {
             text=""
             imageUrl={matches ? "/services-1-web.png" : "/services-1.png"}
             divClassName="landscape:mx-0 landscape:ml-auto"
-
           />
           <SinglePhoto
             title="business consulting services"
